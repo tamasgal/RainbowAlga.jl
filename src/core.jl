a9178111789cc7be6b8360acdd109be8b329b894
Tamas Gal <tamas.gal@fau.de>
2024-06-12 15:30:33 +0200

    Fix typo

--- src/core.jl
+++ src/core.jl
@@ -195,7 +195,7 @@ function basegrid!(scene; center=(0, 0, 0), span=(-500, 500), spacing=50, linewi
     center = Point3f(center)
     for q ∈ range(min, max; step=spacing)
         lines!(scene, [Point3f(q, min, 0) + center, Point3f(q, max, 0) + center], color=color, linewidth=linewidth)
-        lines!(scene, [Point3f(min, q, 0) + center, Point3f(max, q, 0) + center], color=color, linewidht=linewidth)
+        lines!(scene, [Point3f(min, q, 0) + center, Point3f(max, q, 0) + center], color=color, linewidth=linewidth)
     end
     scene
 end

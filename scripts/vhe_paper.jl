dac7f77812ffd7771e99262aa51b4d031a92ca81
Tamas Gal <tamas.gal@fau.de>
2025-03-04 11:07:59 +0100

    Cleanup

--- scripts/vhe_paper.jl
+++ scripts/vhe_paper.jl
@@ -23,7 +23,7 @@ function main()
 
     event = first(f.offline)
     muon = bestjppmuon(event)
-    #hits = select_hits(event.hits, muon)
+    #hits = select_cherenkov_hits(event.hits, muon)
     hits = filter(h->h.dom_id != 808950076 && h.channel_id != 3, event.hits)
     hits = select_first_hits(hits; n=5, maxtot=256)
 
@@ -115,45 +115,5 @@ function main()
     println("Colorbar saved separately as: colorbar.pdf and colorbar_alt.pdf (and .png)")
 end
 
-
-"""
-
-Select hits within a specific Cherenkov time window with respect to the given track.
-
-"""
-function select_hits(hits::T, track; mintot=20, maxtot=255, tmin=-50, tmax=500) where T
-    hits = filter(h -> mintot < h.tot < maxtot, hits)
-    out = T()
-    for hit in hits
-        chit = cherenkov(track, hit)
-        if tmin < chit.Δt < tmax
-            push!(out, hit)
-        end
-    end
-    sort(out; by=h->h.t)
-end
-
-
-"""
-
-Select the first `n` hits on each PMT.
-
-"""
-function select_first_hits(hits::T; n=1, mintot=20, maxtot=255) where T
-    hits = filter(h -> mintot < h.tot < maxtot, hits)
-    out = T()
-    sort!(hits; by=h->h.t)
-    pmts = Dict{Tuple{Int, Int}, Int}()
-    for hit in hits
-        pmtkey = (hit.dom_id, hit.channel_id)
-        if !(pmtkey in keys(pmts))
-            pmts[pmtkey] = 1
-        end
-        pmts[pmtkey] > n && continue
-        pmts[pmtkey] += 1
-        push!(out, hit)
-    end
-    out
-end
 
 main()
